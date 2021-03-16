e30c44a1115af581f98cc834470e01048469b21e
JoseJaraba <jose.jaraba@upb.edu.co>
2021-03-16 18:43:16 -0500

    localstorage

--- js/main.js
+++ js/main.js
@@ -5,6 +5,34 @@ var secciones;
 var modal_is_open = false;
 var nav_history = [];
 
+var registro_usuarios = JSON.parse(localStorage.getItem("usuarios")) == null ? registro_usuarios = [] : JSON.parse(localStorage.getItem("usuarios"));
+var registro_restaurantes = JSON.parse(localStorage.getItem("restaurantes")) == null ? registro_restaurantes = [] : JSON.parse(localStorage.getItem("restaurantes"));
+var current_user = {};
+
+/*Login*/
+var userLogin;
+var passwordLogin;
+
+/*Objeto Usuario*/
+var user;
+var password;
+var confirm_password;
+var security_question;
+var security_answer;
+
+/*Objeto Reserva*/
+var date;
+var hour;
+var number_persons;
+
+/*Objeto Restaurante*/
+var nombre;
+var direccion;
+var telefono;
+var ciudad;
+var time_in;
+var time_out;
+var aforo;
 
 window.onload = () => {
     crearReferencias();
@@ -61,13 +89,40 @@ function crearReferencias() {
     btn_soy_restaurante = document.getElementById("btn_soy_restaurante");
     btn_form_nuevo_restaurante_continuar = document.getElementById("btn_form_nuevo_restaurante_continuar");
     btn_form_nuevo_restaurante_confirmar = document.getElementById("btn_form_nuevo_restaurante_confirmar");
+
+    //Inputs
+    //-Login-//
+    userLogin = document.getElementById("userLogin");
+    passwordLogin = document.getElementById("passwordLogin");
+
+    //-Registro-//
+    user = document.getElementById("user");
+    password = document.getElementById("password");
+    confirm_password = document.getElementById("confirm_password");
+    security_question = document.getElementById("security_question");
+    security_answer = document.getElementById("security_answer");
+
+    //-Restaurante-//
+    nombre = document.getElementById("nombre");
+    direccion = document.getElementById("direccion");
+    telefono = document.getElementById("telefono");
+    ciudad = document.getElementById("ciudad");
+    time_in = document.getElementById("time_in");
+    time_out = document.getElementById("time_out");
+    aforo = document.getElementById("aforo");
+
+    //-Reserva-//
+    date = document.getElementById("date");
+    hour = document.getElementById("hour");
+    number_persons = document.getElementById("number_persons");
+
 }
 
 function agregarEventos() {
-    btn_ingresar.addEventListener("click", () => { irA(lista_restaurantes); });
+    btn_ingresar.addEventListener("click", () => { inicioSesion() });
     btn_reset.addEventListener("click", () => { irA(reset); });
     btn_registro.addEventListener("click", () => { irA(registro); });
-    btn_confirm_login.addEventListener("click", () => { irA(login_screen); });
+    btn_confirm_login.addEventListener("click", () => { agregarUsuario(), irA(login_screen); });
     btn_confirm_login_2.addEventListener("click", () => { irA(login_screen); });
     btn_confirm_login_3.addEventListener("click", () => { irA(login_screen); });
     btn_cont_reset.addEventListener("click", () => { irA(reset_part2); });
@@ -81,7 +136,7 @@ function agregarEventos() {
         }
     });
     btn_confirmar_reserva_1.addEventListener("click", () => { irA(reserva_2) })
-    btn_confirmar_reserva_2.addEventListener("click", () => { irA(lista_restaurantes) })
+    btn_confirmar_reserva_2.addEventListener("click", () => { agregarReserva(), irA(lista_restaurantes) })
 
     btn_soy_restaurante.addEventListener("click", () => {
 
@@ -100,11 +155,10 @@ function agregarEventos() {
         modal();
     });
 
-    btn_form_nuevo_restaurante_continuar.addEventListener("click", () => { irA(form_nuevo_restaurante_2); });
+    btn_form_nuevo_restaurante_continuar.addEventListener("click", () => { agregarRestauranteParte1(); });
     btn_form_nuevo_restaurante_confirmar.addEventListener("click", () => {
-        admin_login();
-        irA(restaurante)
-    })
+        agregarRestauranteParte2(), admin_login(), irA(restaurante);
+    });
 
 
     btn_nav_menu.addEventListener("click", () => { modal(); });
@@ -194,4 +248,123 @@ function logout() {
     }
     is_auth = false;
     irA(login_screen);
+}
+
+function agregarUsuario() {
+    // Validación de Campos vacios
+    if (user.value == "" || password.value == "" || confirm_password.value == "" || security_question.value == "" || security_answer.value == "") {
+        alert("Se deben de llenar todos los campos");
+        return false;
+    }
+    // Valiadación contraseña
+    if (password.value != confirm_password.value) {
+        alert("Las contraseñas deben se ser iguales");
+        return false;
+    }
+
+    var usuario = {};
+    usuario.name = user.value;
+    usuario.password = password.value;
+    usuario.confirm_password = confirm_password.value;
+    usuario.security_question = security_question.value;
+    usuario.security_answer = security_answer.value;
+    usuario.reserva = null;
+
+    //Validación de la existencia de usuarios iguales
+    for (var i in registro_usuarios) {
+        if (registro_usuarios[i].name == user.value) {
+            alert("Ya existe un usuario con ese nombre. Por favor asigne otro nombre");
+            return false;
+        }
+    }
+    registro_usuarios.push(usuario);
+    current_user = usuario;
+    localStorage.setItem("usuarios", JSON.stringify(registro_usuarios));
+
+    //Limpieza inputs//
+    user.value = "";
+    password.value = "";
+    confirm_password.value = "";
+    security_answer.value = "";
+}
+
+function inicioSesion() {
+    //Validación campos
+    if (userLogin.value == "" || passwordLogin.value == "") {
+        alert("Ingrese un usario y una constraseña");
+        return false;
+    }
+    //Validación del usuario
+    for (var i in registro_usuarios) {
+        if (registro_usuarios[i].name === userLogin.value && registro_usuarios[i].password === passwordLogin.value) {
+            current_user = registro_usuarios[i];
+            irA(lista_restaurantes);
+
+            //Limpieza inputs//
+            userLogin.value = "";
+            passwordLogin.value = "";
+
+            return true;
+        }
+    }
+    alert("El usuario y/o la contraseña son incorrectos");
+
+
+}
+
+function agregarReserva() {
+    if (date.value == "" || hour.value == "" || number_persons.value == "") {
+        alert("Se deben de llenar todos los campos");
+        return false;
+    }
+    var reserva = {}
+    reserva.date = date.value;
+    reserva.hour = hour.value;
+    reserva.number_persons = number_persons.value;
+
+    current_user.reserva = reserva;
+
+    for (var i in registro_usuarios) {
+        if (registro_usuarios[i].name == current_user.name) {
+            registro_usuarios[i] = current_user;
+            localStorage.setItem("usuarios", JSON.stringify(registro_usuarios));
+            return true;
+        }
+    }
+
+}
+
+function agregarRestauranteParte1() {
+    // Validación de Campos vacios
+    if (nombre.value == "" || direccion.value == "" || telefono.value == "" || ciudad.value == "") {
+        alert("Se deben de llenar todos los campos");
+        return false;
+    }
+    irA(form_nuevo_restaurante_2);
+}
+function agregarRestauranteParte2() {
+    if (time_in.value == "" || time_out.value == "" || aforo.value == "") {
+        alert("Se deben de llenar todos los campos");
+        return false;
+    }
+    var restaurante = {};
+    restaurante.name = nombre.value;
+    restaurante.address = direccion.value;
+    restaurante.phoneNumber = telefono.value;
+    restaurante.city = ciudad.value;
+    restaurante.time_in = time_in.value;
+    restaurante.time_out = time_out.value;
+    restaurante.amount = aforo.value;
+
+    registro_restaurantes.push(restaurante);
+    localStorage.setItem("restaurantes", JSON.stringify(registro_restaurantes));
+
+    nombre.value = "";
+    direccion.value = "";
+    telefono.value = "";
+    ciudad.value = "";
+    time_in.value = "";
+    time_out.value = "";
+    aforo.value = "";
+
 }
